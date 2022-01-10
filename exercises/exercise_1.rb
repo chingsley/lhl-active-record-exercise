807397c215464a14f02e29ed158fd2c387b4049d
Kingsley Eneja <eneja.kc@gmail.com>
2022-01-11 00:01:42 +0100

    - Quering active records where condiation

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,7 +8,7 @@ puts "----------"
 # # Your code goes below here ...
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 store2 = Store.create(name: "Richmond", annual_revenue: 260000, mens_apparel: false, womens_apparel: true)
-store1 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 # puts store1
 # puts store1.name
 # puts store1.annual_revenue
