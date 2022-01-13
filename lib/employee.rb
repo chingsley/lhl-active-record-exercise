e750120a6ca82a1c358b15cd2d8689d4be97811b
Kingsley Eneja <eneja.kc@gmail.com>
2022-01-13 17:40:04 +0100

    - Validation with AR callbacks

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,14 @@
 class Employee < ActiveRecord::Base
   belongs_to :store, validate: true
-
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, :inclusion => 40..200
+  
+  before_create :set_password
+  
+  private
+  def set_password
+    self.password = (0...8).map { (65 + rand(26)).chr }.join
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -5,9 +5,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
   validate :stores_must_carry_at_least_one_of_mens_or_womens_apparel
 
+  before_destroy :ensure_cannot_destroy_store_that_has_employees
+
   def stores_must_carry_at_least_one_of_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:a_store, "must carry at least one of men's or women's apparel") #will print as  "A store must carry at least one of men's or women's apparel"
     end
+  end
+
+  def ensure_cannot_destroy_store_that_has_employees
+    if self.employees.size > 0
+      puts "\n\ncannot delete store that has associated employees\n\n"
+      throw :abort
+    end
+  rescue
+    false
   end
 end
